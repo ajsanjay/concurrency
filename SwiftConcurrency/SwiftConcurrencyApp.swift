667cef4e5d6f1210bf8758c73cbc2227554650a0
Jaya Sabeen <jayasabeen@Jayas-MacBook-Pro.local>
2025-01-29 10:57:51 +0530

    first commit

---
+++ SwiftConcurrency/SwiftConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConcurrencyApp.swift
+//  SwiftConcurrency
+//
+//  Created by Jaya Sabeen on 29/01/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
